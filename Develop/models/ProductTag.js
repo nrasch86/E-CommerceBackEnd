96835bfb1dd1616ed294ad5b0f331b231c9443af
nrasch86 <145396887+nrasch86@users.noreply.github.com>
2024-01-11 22:44:27 -0500

    Initial commit

---
+++ Develop/models/ProductTag.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', 
+        key: 'id',
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', 
+        key: 'id',
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'productTag',
+  }
+);
+
+module.exports = ProductTag;
---
+++ README.md
@@ -0,0 +1,16 @@
+# E-commerce Back End Code
+
+The code housed in this repository was developed for the Columbia Coding Bootcamp Module 13 challenge.
+The objective of this project is to be able to test the developed database through the use of Post, Put and Delete routes within Insomnia Core.
+
+Code & Software used: JavaScript, Sequelize, Insomnia
+
+
+Resources Used:
+-Starter Code
+https://www.npmjs.com/package/mysql2#installation
+https://sequelize.org/docs/v6/getting-started/
+https://www.npmjs.com/package/mysql#install
+https://docs.insomnia.rest/
+https://www.npmjs.com/package/dotenv
+
